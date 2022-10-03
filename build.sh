5c91d6f4a651aa55ea2f43096f140a838dd54062
lanre <lanre@User.pc>
2022-10-03 23:37:47 +0100

    hmm*2

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 # exit on error
 set -o errexit
 
-# poetry install
 
-python manage.py migrate
+poetry install
+pip install --upgrade pip
+pip install --force-reinstall -U setuptools
 python manage.py collectstatic --no-input
+python manage.py migrate
